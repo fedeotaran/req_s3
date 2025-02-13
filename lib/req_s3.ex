1724a1acca30d86c165d685e4b6d11b445c5aaeb
Fede Otaran <otaran.federico@gmail.com>
2025-02-13 16:32:09 +0100

    Allow s3_acelerate option for presing_url

--- lib/req_s3.ex
+++ lib/req_s3.ex
@@ -95,6 +95,8 @@ defmodule ReqS3 do
     * `:endpoint_url` - if set, the endpoint URL for S3-compatible services. If
       `AWS_ENDPOINT_URL_S3` system environment variable is set, it is considered first.
 
+    * `:s3_accelerate` - if set, use the S3 Accelerate endpoint.
+
   ## Examples
 
   Note: This example assumes `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` environment variables
@@ -131,17 +133,23 @@ defmodule ReqS3 do
       key = Keyword.fetch!(options, :key)
 
       endpoint_url = options[:endpoint_url] || System.get_env("AWS_ENDPOINT_URL_S3")
+      accelerate = options[:accelerate]
 
-      if endpoint_url do
-        "#{endpoint_url}/#{bucket}/#{key}"
-      else
-        "https://#{bucket}.s3.amazonaws.com/#{key}"
+      cond do
+        endpoint_url ->
+          "#{endpoint_url}/#{bucket}/#{key}"
+
+        accelerate ->
+          "https://#{bucket}.s3-accelerate.amazonaws.com/#{key}"
+
+        true ->
+          "https://#{bucket}.s3.amazonaws.com/#{key}"
       end
     end)
     |> Keyword.update!(:url, &normalize_url(&1, options[:endpoint_url]))
     |> Keyword.put(:service, "s3")
-    |> Keyword.put_new(:datetime, DateTime.utc_now())
-    |> Keyword.drop([:bucket, :key, :endpoint_url])
+    |> Keyword.put(:datetime, DateTime.utc_now())
+    |> Keyword.drop([:bucket, :key, :endpoint_url, :accelerate])
     |> Req.Utils.aws_sigv4_url()
     |> URI.to_string()
   end
@@ -381,7 +389,7 @@ defmodule ReqS3 do
 
         url
 
-      [_ | _]  ->
+      [_ | _] ->
         # bucket has dots in it
         bucket = url.host
 
